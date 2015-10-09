03a9c7b232b7a04b49a821b05bbdf38eb49c7761
Mariama <mcso@cin.ufpe.br>
2015-10-09 13:22:01 -0300

    loading file

--- projectFile.c
+++ projectFile.c
@@ -1,7 +1,76 @@
 #include <stdio.h>
- 
+
 int main()
 {
-  printf("Hello world\n");
-  return 0;
+
+//File variables
+FILE *ifp;
+char *mode="r";
+char *fileName="tic-tac-toe.data";
+
+//Data Structure that will receive the file info
+int data [958][10];
+
+
+//////////////opening and reading file////////////////////
+
+ifp= fopen(fileName, mode);
+
+//Verifing if the file was open correctly
+if(ifp!=NULL){
+	printf("File successfully opened!\n");
+
+	//Reading each character of the opened file
+	char ch;
+	int i=0;
+	int j=0;
+	int insertedValue;
+
+	while( ( ch = fgetc(ifp) ) != EOF ){
+
+		if(ch=='x' || ch=='o' || ch=='b' || ch=='p' || ch=='n'){ //ignore any other character
+
+			//load the file into the selected data structure
+			if(ch=='x'){
+				insertedValue= 1;
+
+			}else if(ch=='o'){
+				insertedValue= 0;
+
+			}else if(ch=='b'){
+				insertedValue= -1;
+
+			}else if(ch=='p'){
+				insertedValue= 1;
+				fgetc(ifp);
+
+			}else{
+				insertedValue= 0;
+
+			}
+			
+			data[i][j]=insertedValue;
+			j++;
+
+			//adjusting i and j
+			if(j==10){
+				j=0;
+				i++;
+			}
+
+		}
+	      
+	}
+
+	fclose(ifp);
+	
+}else{
+	printf("The file was not open!\n");
+}
+
+//////////////////////data structure uploaded//////////////////
+
+
+
+return 0;
 }
